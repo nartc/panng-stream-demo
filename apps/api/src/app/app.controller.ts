f3ffd509c86853563927502ac0785d39b2b98aff
Chau Tran <nartc7789@gmail.com>
2020-12-19 15:19:32 -0600

    chore: remove interfaces lib

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,15 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 
-import { Message } from '@panng-stream/api-interfaces';
-
-import { AppService } from './app.service';
-
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get('hello')
-  getData(): Message {
-    return this.appService.getData();
+  getData() {
+    return 'hello';
   }
 }
--- apps/api/src/app/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Message } from '@panng-stream/api-interfaces';
-
-@Injectable()
-export class AppService {
-  getData(): Message {
-    return { message: 'Welcome to api!' };
-  }
-}
